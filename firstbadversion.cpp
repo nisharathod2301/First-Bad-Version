04a6df1f44103dee7476da92e40e14e2ae38ee68
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2022-08-14 23:56:23 +0530

    Create firstbadversion.cpp

---
+++ firstbadversion.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1, high = n, mid;
+        
+        while(low <= high) {
+            mid = low + (high - low) / 2;
+            if(isBadVersion(mid)) 
+                high = mid - 1;
+            else 
+                low = mid + 1;
+        }
+        return low;
+    }
+};
